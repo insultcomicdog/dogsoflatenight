e9dc91e96afc6cc35bb30e41961255f58ff03b51
Sunil <sunil@jam3media.com>
2012-04-15 17:14:20 -0400

    - added text to speech functionality…still having issues converting strings to ints or floats

--- src/dogsoflatenight/dogsoflatenight.ino
+++ src/dogsoflatenight/dogsoflatenight.ino
@@ -6,9 +6,23 @@
 
 #include <TextFinder.h>
 
-char twitterSearchTerms[] = "insultcomicdog%20OR%20formetopoopon";
-int since_id = 0;
-int totalnumsearchresults = 0;
+//set up a SoftwareSerial port for Speakjet Shield
+//rxPin: the pin on which to receive serial data from TTS256
+//txPin: the pin on which to transmit serial data to the TTS256
+#include <SoftwareSerial.h>
+#define txPin 2
+#define rxPin 3
+SoftwareSerial sjSerial = SoftwareSerial(rxPin, txPin);
+
+
+//this is the service i'm calling
+//http://www.suniljohn.com/labs/dogsoflatenight/twitterpoll.php?since_id=0&track=insultcomicdog%20OR%20formetopoopon%20OR%20for%20me%20to%20poop%20on%20-RT
+
+char twitterSearchTerms[] = "insultcomicdog%20OR%20formetopoopon%20OR%20for%20me%20to%20poop%20on%20-RT";
+
+
+float since_id = 0;
+float totalnumsearchresults = 0;
 
 char totalnumsearchresults_str[200];
 char from_user[20];
@@ -41,9 +55,28 @@ void setup() {
   }  
 
   Serial.println("Associated!");
+  
+  initSpeakJet();
 
    // connect to Twitter:
   delay(3000);
+  
+}
+
+void initSpeakJet()
+{
+ // initialize the serial communications with the SpeakJet-TTS256
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  sjSerial.begin(9600);// set the data rate for the SoftwareSerial port
+  delay(1000); // wait a second for the Arduino resets to finish (speaks "ready")
+  sjSerial.println("The Dogs of Late Night is now online. All your poop are belong to us."); // send it to the SpeakJet
+}
+
+
+//TODO: Initialize WAV shield when Twitter searches yeild 0 results
+void initWaveSheild()
+{
   
 }
 
@@ -65,7 +98,9 @@ void loop() {
 //      Serial.println();
 
     client.print("GET /labs/dogsoflatenight/twitterpoll.php?since_id=");
-    client.print(since_id);
+    client.print(since_id); //this works
+    //client.print("0"); //this works
+    //client.print(since_id_str); //this launches board into rest cycle
     client.print("&track=");
     client.print(twitterSearchTerms);
     client.println(" HTTP/1.1");
@@ -73,6 +108,7 @@ void loop() {
     client.println();
     
     Serial.println("sent HTTP request...");
+    sjSerial.println("Polling Twitter for poop.");
     
     while (client.connected()) {
     if (client.available()) {
@@ -85,8 +121,6 @@ void loop() {
       if((finder.getString("<source>","</source>",source,200)!=0));  
       if((finder.getString("<created_at>","</created_at>",created_at,200)!=0));
       if((finder.getString("<since_id>","</since_id>",since_id_str,200)!=0));
-      
-     
     }
   }
 }
@@ -107,8 +141,11 @@ void loop() {
   //totalnumsearchresults = stringToNumber(totalnumsearchresults_str); //this works
   //since_id = stringToNumber(since_id_str); //this doesn't work...189148246403325952 ---> 8192
   
-  totalnumsearchresults = getInt(totalnumsearchresults_str); //this works
-  since_id = getInt(since_id_str); //this kind of works but doesn't convert full int...189148246403325952 ---> 1891
+  //totalnumsearchresults = getInt(totalnumsearchresults_str); //this works
+  //since_id = getInt(since_id_str); //this kind of works but doesn't convert full int...189148246403325952 ---> 1891
+  
+  float totalnumsearchresults = atof(totalnumsearchresults_str);
+  float since_id = atof(since_id_str);
   
   
   Serial.println(totalnumsearchresults_str);
@@ -121,7 +158,13 @@ void loop() {
   
   Serial.println(totalnumsearchresults);
   Serial.println(since_id);
-
+  
+  if(totalnumsearchresults!=0){
+      readTweet(tweet);
+  } else{
+      //there are no results for my keywords so lets switch the motion sensor/WAV shield here  
+  }
+  
   Serial.println();
   Serial.println("delay...");
   delay (60000); 
@@ -129,21 +172,26 @@ void loop() {
   // off course it would be better to use the "Blink without delay", but I leave that to you.
 }
 
-
-int stringToNumber(String thisString) {
-  int i, value = 0, length;
-  length = thisString.length();
-  for(i=0; i<length; i++) {
-    value = (10*value) + thisString.charAt(i)-(int) '0';
-  }
-  return value;
+void readTweet(String thisString)
+{
+  sjSerial.println(thisString); 
 }
 
-int getInt(String text)
-{
-  char temp[6];
-  text.toCharArray(temp, 5);
-  int x = atoi(temp);
-  return x;
-} 
+
+//int stringToNumber(String thisString) {
+//  int i, value = 0, length;
+//  length = thisString.length();
+//  for(i=0; i<length; i++) {
+//    value = (10*value) + thisString.charAt(i)-(int) '0';
+//  }
+//  return value;
+//}
+//
+//int getInt(String text)
+//{
+//  char temp[6];
+//  text.toCharArray(temp, 5);
+//  int x = atoi(temp);
+//  return x;
+//} 
 
