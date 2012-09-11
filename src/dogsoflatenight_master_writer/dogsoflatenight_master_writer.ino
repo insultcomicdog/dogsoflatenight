3f178fdad759cd770963b4aa073b52f1e3a9b970
Sunil <sunil@jam3media.com>
2012-09-11 10:58:38 -0400

    - added a timeout counter for Twitter API requests - fine tuned Twitter search - added support for displaying data on Nixie counter!

--- src/dogsoflatenight_master_writer/dogsoflatenight_master_writer.ino
+++ src/dogsoflatenight_master_writer/dogsoflatenight_master_writer.ino
@@ -30,10 +30,10 @@ Thermal printer(printer_RX_Pin, printer_TX_Pin, 19200);
 
 
 //this is the service i'm calling
-//http://www.suniljohn.com/labs/dogsoflatenight/twitterpoll.php?since_id=0&track=insultcomicdog%20OR%20formetopoopon%20OR%20%22for%20me%20to%20poop%20on%22%20-RT&rpp=3
+//http://www.suniljohn.com/labs/dogsoflatenight/twitterpoll.php?since_id=0&track=insultcomicdog%20OR%20formetopoopon%20OR%20%22for%20me%20to%20poop%20on%22%20-RT&rpp=1
 
-char twitterSearchTerms[] = "insultcomicdog%20OR%20formetopoopon%20OR%20%22for%20me%20to%20poop%20on%22%20-RT";
-char current_since_id_str[20] = "0";
+char twitterSearchTerms[] = "%22insult%20comic%20dog%22%20OR%20formetopoopon%20OR%20%22for%20me%20to%20poop%20on%22%20-RT";
+char current_since_id_str[20];
 char next_since_id_str[20];
 
 int totalnumsearchresults = 0;
@@ -59,6 +59,9 @@ boolean speakJetIsBusy = false;
 
 byte slaveMode = 0;
 int motionCounter= 0;
+int responseTimeoutCounter= 0;
+int maxResponseTimeOut = 5000;
+
 
 unsigned long currentTime;
 unsigned long loopTime;
@@ -77,9 +80,16 @@ int statusMotionDectionModeGreen = 41; //green
 int statusMotionDectionModeRed = 45; //red
 
 
+//Nixie Counter Vars
+int tweets = 0;
+char buffer[7];
+
+
 void setup()
 {
   Serial.begin(9600); 
+  Serial2.begin(9600); // for nixie counter
+  resetTweetCounter();
   initStatusLEDs();
   setLEDStatus(STATUS_WIFI_DISCONNECTED);
   Wire.begin(); // join i2c bus (address optional for master)
@@ -102,7 +112,8 @@ void setup()
   initSpeakJet();
   
   currentTime = millis();
-  loopTime = currentTime;  
+  loopTime = currentTime;
+  current_since_id_str[0]='0'; 
 }
 
 void initStatusLEDs()
@@ -187,6 +198,7 @@ void loop()
           Serial.println("twitter search enabled");
           sjSerial.println("Serching Twitter for poop."); //this is not a typo...it actually sounds better read by the SpeakJet shield
           isSearching=true;
+          showTweetCounter();
         }
        
         if(currentTime >= (loopTime + 60000)){ 
@@ -208,10 +220,12 @@ void loop()
 //          Serial.println(" HTTP/1.1");
 //          Serial.println("HOST: www.suniljohn.com");
 //          Serial.println();
+          Serial.println("current_since_id_str");
+          Serial.println(current_since_id_str);
       
           
           client.print("GET /labs/dogsoflatenight/twitterpoll.php?since_id=");
-          client.print(current_since_id_str); //this launches board into reset cycle
+          client.print(current_since_id_str);
           client.print("&track=");
           client.print(twitterSearchTerms);
           client.print("&rpp=");
@@ -219,23 +233,34 @@ void loop()
           client.println(" HTTP/1.1");
           client.println("HOST: www.suniljohn.com");
           client.println();
-          
+                    
           Serial.println("sent HTTP request...");
-         
           
           while (client.connected()) {
-          if (client.available()) {
-            //char c = client.read();
-            //Serial.print(c);
-            if((finder.getString("<totalnumsearchresults>","</totalnumsearchresults>",totalnumsearchresults_str,200)!=0));
-            if((finder.getString("<from_user>","</from_user>",from_user,20)!=0));
-            if((finder.getString("<profile_image_url>","</profile_image_url>",profile_image_url, 200)!=0));
-            if((finder.getString("<text>","</text>",tweet,140)!=0));
-            if((finder.getString("<source>","</source>",source,200)!=0));  
-            if((finder.getString("<created_at>","</created_at>",created_at,200)!=0));
-            if((finder.getString("<since_id>","</since_id>",next_since_id_str,20)!=0));
+                  Serial.println("client is connected");
+                  responseTimeoutCounter++;
+                  Serial.println(responseTimeoutCounter);
+                 if(responseTimeoutCounter >= maxResponseTimeOut){ 
+                     Serial.println("we timed out");
+                     responseTimeoutCounter=0;
+                     totalnumsearchresults_str[0] = '0';
+                     delay(1);
+                     client.stop();
+                  }
+                  
+            if (client.available()) {
+                  Serial.println("waiting for response");
+                  //char c = client.read();
+                  //Serial.print(c);
+                  if((finder.getString("<totalnumsearchresults>","</totalnumsearchresults>",totalnumsearchresults_str,200)!=0));
+                  if((finder.getString("<from_user>","</from_user>",from_user,20)!=0));
+                  if((finder.getString("<profile_image_url>","</profile_image_url>",profile_image_url, 200)!=0));
+                  if((finder.getString("<text>","</text>",tweet,140)!=0));
+                  if((finder.getString("<source>","</source>",source,200)!=0));  
+                  if((finder.getString("<created_at>","</created_at>",created_at,200)!=0));
+                  if((finder.getString("<since_id>","</since_id>",next_since_id_str,20)!=0));
+            }
           }
-        }
       }
         
         delay(1);
@@ -243,7 +268,6 @@ void loop()
         client.stop();
         
         totalnumsearchresults = atof(totalnumsearchresults_str);
-       
         Serial.println();
         Serial.println();
         Serial.println("parsed twitter data...");
@@ -256,9 +280,16 @@ void loop()
         Serial.println(current_since_id_str);
         Serial.println(next_since_id_str);
         Serial.println(totalnumsearchresults);
+         
+        Serial.println("is a repeat");
+        Serial.println(strcmp(current_since_id_str, next_since_id_str)); // if this is 0 the two strings are the same
         
-        if(totalnumsearchresults!=0){
-
+        if(totalnumsearchresults!=0 && strcmp(current_since_id_str, next_since_id_str) != 0){ // sometimes twitter api is returning the same since id back as previous search
+            Serial.println("we found a new tweet");
+            
+            tweets++; //increment tweet count for Nixie counter
+            showTweetCounter();
+            
             //disabling this for now to save on paper
             printSRC(created_at); 
             delay(3000);
@@ -271,9 +302,11 @@ void loop()
             
             readTweet(tweet);
       
-            for (int i=0; i <= 20; i++){
-              current_since_id_str[i] = next_since_id_str[i];
-            }
+//            for (int i=0; i <= 20; i++){
+//              current_since_id_str[i] = next_since_id_str[i];
+//            }
+            
+            strncpy(current_since_id_str, next_since_id_str, 20);
             
         } else {
             //there are no new results for my keywords so lets switch the motion sensor/WAV shield here  
@@ -282,6 +315,7 @@ void loop()
             Serial.println("twitter search disabled");
             sjSerial.println("There is no poop on twitter. Switching to motion detection mode.");
             setLEDStatus(STATUS_MOTION_DETECTION_MODE);
+            hideTweetCounter();
         }
         
           Serial.println();
@@ -289,6 +323,9 @@ void loop()
         
           isSearching=false;
           loopTime = currentTime;
+          responseTimeoutCounter=0;
+          totalnumsearchresults=0;
+          totalnumsearchresults_str[0] = '0';
         }
         
         //delay (60000); 
@@ -316,6 +353,22 @@ void loop()
   Wire.endTransmission();    // stop transmitting
   delay(500);
   
+}
+
+void showTweetCounter()
+{
+  sprintf(buffer, "%06d", tweets); 
+  Serial2.println(buffer);
+}
+
+void hideTweetCounter()
+{
+  Serial2.println("b"); //turn off Nixie counter
+}
+
+void resetTweetCounter()
+{
+    Serial2.println("r"); //zero pad the Nixie counter
 }
 
 void readTweet(String thisString)
